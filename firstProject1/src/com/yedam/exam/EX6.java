f8943f7bfd75cc8fc4167e98d2650b6f5d4d607a
duavudrkd123 <ypgcute@gmail.com>
2020-08-13 12:23:05 +0900

    aa

--- firstProject1/src/com/yedam/exam/EX6.java
+++ firstProject1/src/com/yedam/exam/EX6.java
@@ -5,7 +5,7 @@ public class EX6 {
 //		System.out.println("*");
 //		System.out.println("**");
 //		System.out.println("***");
-//		System.out.println("****");
+//		System.out.println("****");e
 //for 문을 사용하여 출력해봐라
 	for (int s = 1; s <= 7; s++) {
 		for (int t = 1; t <= s; t++) {
